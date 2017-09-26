0d71df47632cf357300ceb9bc83ed424f0f229b8
Jake Champion <jakechampion.jake2@googlemail.com>
2017-09-26 16:21:18 +0100

    Move to proclaim

--- test/element-visibility.test.js
+++ test/element-visibility.test.js
@@ -1,5 +1,5 @@
-/*global describe, it, beforeEach, afterEach*/
-const expect = require('expect.js');
+/* eslint-env mocha */
+const proclaim = require('proclaim');
 const sinon = require('sinon');
 
 const oElemVis = require('./../main.js');
@@ -15,6 +15,7 @@ describe('o-element-visibility', function() {
 	const height = (window.innerHeight + 100);
 	beforeEach(function() {
 		document.body.style.height = height + 'px';
+		document.body.style.margin = '0';
 		document.body.insertAdjacentHTML('afterbegin', '<div id="inview" style="width: 10px; height: 10px; background: #bada55;" data-o-element-visibility-track></div>');
 		document.body.insertAdjacentHTML('afterbegin', '<div id="outview" style="width: 10px; height: 10px; top: ' + window.innerHeight + 'px; position: absolute; background: #bada55;"></div>');
 		inview = document.getElementById('inview');
@@ -33,36 +34,36 @@ describe('o-element-visibility', function() {
 
 	it('should track elements with the attribute  data-o-element-visibility-track', function() {
 		document.documentElement.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-		expect(oElemVis.tracked.length).to.equal(1);
+		proclaim.equal(oElemVis.tracked.length, 1);
 	});
 
 	it('should track stop tracking elements when destroy is called', function() {
 		oElemVis.track(inview);
-		expect(oElemVis.tracked.length).to.equal(1);
+		proclaim.equal(oElemVis.tracked.length, 1);
 		oElemVis.destroy();
-		expect(oElemVis.tracked.length).to.equal(0);
+		proclaim.equal(oElemVis.tracked.length, 0);
 	});
 
 	it('Attempting to track the same element twice is caught', function() {
 		const result1 = oElemVis.track(inview);
-		expect(oElemVis.tracked.length).to.equal(1);
+		proclaim.equal(oElemVis.tracked.length, 1);
 		const result2 = oElemVis.track(inview);
-		expect(result1).to.eql(result2);
-		expect(oElemVis.tracked.length).to.equal(1);
+		proclaim.deepEqual(result1, result2);
+		proclaim.equal(oElemVis.tracked.length, 1);
 	});
 
 	it('should track elements passed to track', function() {
 		oElemVis.track(outview);
-		expect(oElemVis.tracked.length).to.equal(1);
-		expect(oElemVis.tracked[0].node.id).to.be('outview');
+		proclaim.equal(oElemVis.tracked.length, 1);
+		proclaim.equal(oElemVis.tracked[0].node.id, 'outview');
 	});
 
 	describe('should fire events when first run', function() {
 		it('should report the inview element as 100% in viewport', function(done) {
 			function assert(event) {
-				expect(event.detail.inviewport).to.be(true);
-				expect(event.detail.percentage).to.be(100);
-				expect(event.detail.element.node.id).to.be('inview');
+				proclaim.equal(event.detail.inviewport, true);
+				proclaim.equal(event.detail.percentage, 100);
+				proclaim.equal(event.detail.element.node.id, 'inview');
 
 				document.documentElement.removeEventListener('oVisibility.inview', assert);
 				done();
@@ -81,9 +82,9 @@ describe('o-element-visibility', function() {
 			}
 
 			function assert(event) {
-				expect(event.detail.inviewport).to.be(false);
-				expect(event.detail.percentage).to.be(0);
-				expect(event.detail.element.node.id).to.be('outview');
+				proclaim.equal(event.detail.inviewport, false);
+				proclaim.equal(event.detail.percentage, 0);
+				proclaim.equal(event.detail.element.node.id, 'outview');
 
 				document.documentElement.removeEventListener('oVisibility.inview', assert);
 				done();
@@ -108,22 +109,22 @@ describe('o-element-visibility', function() {
 			// spy on the update position
 			let trackedElementUpdatePositionSpy = sinon.spy(trackedElement, 'updatePosition');
 
-			expect(trackedElement.top).to.equal(0);
-			expect(trackedElement.inview).to.equal(true);
-			expect(trackedElementUpdatePositionSpy.callCount).to.equal(0);
+			proclaim.equal(trackedElement.top, 0);
+			proclaim.equal(trackedElement.inview, true);
+			proclaim.equal(trackedElementUpdatePositionSpy.callCount, 0);
 			// append DOM to add a new element
 			document.body.insertAdjacentHTML('afterbegin', '<div id="gap" style="width: 10px; height: '+height+'px; background: #ff0000;"></div>');
 			function assertFirst(){
 				// make sure tracked element position has been updated
-				expect(trackedElement.top).to.equal(height);
-				expect(trackedElement.inview).to.equal(false);
+				proclaim.equal(trackedElement.top, height);
+				proclaim.equal(trackedElement.inview, false);
 				// make sure the recalculation has only been done once and not once for each scroll event
-				expect(trackedElementUpdatePositionSpy.callCount).to.equal(1);
+				proclaim.equal(trackedElementUpdatePositionSpy.callCount, 1);
 			}
 
 			function assertSecond(){
 				// make sure we did not call the updatePosition twice
-				expect(trackedElementUpdatePositionSpy.callCount).to.equal(1);
+				proclaim.equal(trackedElementUpdatePositionSpy.callCount, 1);
 				done();
 			}
 
@@ -158,12 +159,12 @@ describe('o-element-visibility', function() {
 
 			function assertFirst(event) {
 				if (event.detail.element.node.id === 'inview') {
-					expect(event.detail.inviewport).to.be.true;
-					expect(event.detail.percentage).to.be(100);
+					proclaim.equal(event.detail.inviewport, true);
+					proclaim.equal(event.detail.percentage, 100);
 					assertions++;
 				} else if (event.detail.element.node.id === 'outview') {
-					expect(event.detail.inviewport).to.be.false;
-					expect(event.detail.percentage).to.equal(0);
+					proclaim.equal(event.detail.inviewport, false);
+					proclaim.equal(event.detail.percentage, 0);
 					assertions++;
 				}
 
@@ -177,12 +178,12 @@ describe('o-element-visibility', function() {
 
 			function assertSecond(event) {
 				if (event.detail.element.node.id === 'inview') {
-					expect(event.detail.inviewport).to.be.false;
-					expect(event.detail.percentage).to.be(0);
+					proclaim.equal(event.detail.inviewport, false);
+					proclaim.equal(event.detail.percentage, 0);
 					assertions++;
 				} else if (event.detail.element.node.id === 'outview') {
-					expect(event.detail.inviewport).to.be.true;
-					expect(event.detail.percentage).to.be(100);
+					proclaim.equal(event.detail.inviewport, true);
+					proclaim.equal(event.detail.percentage, 100);
 					assertions++;
 				}
 
